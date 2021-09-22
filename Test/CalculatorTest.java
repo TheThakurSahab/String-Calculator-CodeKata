f477e505408907b58f6e693f9625fce2f1d930a7
Akash Singh <akashsingh.work@outlook.com>
2021-09-22 15:59:40 +0530

    Adding test to handle multiple number seperated by comma

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -16,4 +16,9 @@ public class CalculatorTest {
         assertEquals(8, Calculator.Add("5,3"));
         assertEquals(10, Calculator.Add("7,3"));
     }
+    @Test
+    public void shouldReturnSumIfStringHaveMultipleNumber(){
+        assertEquals(12,Calculator.Add("6,4,2"));
+        assertEquals(15,Calculator.Add("6,5,4"));
+    }
 }
