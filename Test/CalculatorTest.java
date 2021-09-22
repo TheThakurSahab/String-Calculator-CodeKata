3cd83902b5778b442dd8416bb034fd47faa3f39c
Akash Singh <akashsingh.work@outlook.com>
2021-09-22 15:58:28 +0530

    Adding more test cases

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -9,9 +9,11 @@ public class CalculatorTest {
     @Test
     public void shouldReturnNumberIfStringHaveNumber(){
         assertEquals(5,Calculator.Add("5"));
+        ssertEquals(1,Calculator.Add("1"));
     }
     @Test
     public void shouldReturnSumIfStringHaveTwoNumberDelimitedByComma(){
         assertEquals(8, Calculator.Add("5,3"));
+        assertEquals(10, Calculator.Add("7,3"));
     }
 }
